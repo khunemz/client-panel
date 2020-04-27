b745754c96535103821f5402589ad72b8867bfd2
Chutipong Roobklom <khunemz@gmail.com>
2020-04-28 03:50:57 +0700

    feat/add fa

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,24 +1,41 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
+import React, { Component } from "react";
+import PropTypes from "prop-types";
 
 class Contact extends Component {
-    
-        render() {
-        let contact = this.props.contact;
+    state = {
+        showContactInfo: false 
+    };
 
-        return (
-            <div className="card card-body mb-3">
-                <h4>{contact.name}</h4>
-                <ul className="list-group">
-                    <li className="list-group-item">{contact.email}</li>
-                    <li className="list-group-item">{contact.phone}</li>
-                </ul>
-            </div>
-        )
+    toggleAccordian = ( e ) => {
+        this.setState({
+            showContactInfo: !this.state.showContactInfo
+        })
+
     }
+
+  render() {
+    let contact = this.props.contact;
+    const {showContactInfo} = this.state;
+    return (
+      <div className="card card-body mb-3">
+        <h4>
+          {contact.name} 
+          <i className="fas fa-sort-down" onClick={this.toggleAccordian}></i>
+        </h4>
+
+        {
+            showContactInfo ? (<ul className="list-group">
+            <li className="list-group-item">{contact.email}</li>
+            <li className="list-group-item">{contact.phone}</li>
+          </ul>) : null 
+        }
+        
+      </div>
+    );
+  }
 }
 
 Contact.propTypes = {
-    contact: PropTypes.object.isRequired
-}
+  contact: PropTypes.object.isRequired,
+};
 export default Contact;
